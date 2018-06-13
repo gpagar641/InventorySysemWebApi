4f0e820b939978b9e1a3d63f6c941911f7aff4a9
BIONISCHE <BIONISCHE@DESKTOP-JDU67KT>
2018-06-13 16:41:23 +0530

    //Ganesh

---
+++ src/main/java/com/gensoft/inventory/InventorySystemWebApiApplication.java
@@ -0,0 +1,12 @@
+package com.gensoft.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySystemWebApiApplication.class, args);
+	}
+}
